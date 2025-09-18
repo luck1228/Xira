24ffe2fec0c5e761770b2f5fc9b45cba2a2473f4
Wojciech Henik <luck122@live.com>
2025-09-18 14:27:54 +0200

    Added a SecurityConfig to disable security for early dev

---
+++ backend/src/main/java/io/xira/xira/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package io.xira.xira.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests((requests) -> requests
+                        .anyRequest().permitAll());
+
+        return http.build();
+    }
+}
