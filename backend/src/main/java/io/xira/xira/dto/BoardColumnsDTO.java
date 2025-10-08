20f6e28702ffc2ef90a4d4840cbcedaec7b0106a
Wojciech Henik <luck122@live.com>
2025-10-08 18:34:13 +0200

    Removed unused import

--- backend/src/main/java/io/xira/xira/dto/BoardColumnsDTO.java
+++ backend/src/main/java/io/xira/xira/dto/BoardColumnsDTO.java
@@ -2,7 +2,6 @@ package io.xira.xira.dto;
 
 import jakarta.persistence.Column;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 
 public class BoardColumnsDTO {
 
